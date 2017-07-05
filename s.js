f0175227dc433dc28a3f1c2d175f99d4b8bb97de
CameronSamuels <satchmosanders@gmail.com>
2017-07-05 17:39:24 -0500

    Cleaned up a lot of script
    Removed handleEvent() and replaced with exact functions for events, made certain code one lined with commas, combined some regex, made booleans not strings, more

--- s.js
+++ s.js
@@ -1,69 +1,47 @@
 function a(e) { return document.querySelector(e) }
-var movement = 0, mousedown = 'false', el = a('img'), angle = 0, startAngle = 0, originX = 160, originY = 160,
+var movement = 0, mousedown = false, el = a('img'), angle = 0, startAngle = 0, originX = 160, originY = 160,
   spinner = {
-    handleEvent: function(e) {
-      if (e.type == 'touchstart') this.rotateStart(e);
-      else if (e.type == 'touchmove') this.rotateMove(e);
-      else if (e.type == 'touchend') this.rotateStop(e);
-      else if (e.type == 'mousedown') {
-        mousedown = 'true';
-        this.rotateStart(e);
-      } else if (e.type == 'mousemove' && mousedown == 'true') this.rotateMove(e);
-      else if (e.type == 'mouseup') {
-        mousedown = 'false';
-        this.rotateStop(e);
-      }
-    },
     init: function() {
       originX = innerWidth / 2;
       originY = innerHeight / 2;
       el.style.transitionDuration = '0';
-      el.addEventListener('touchstart', this, false);
-      el.addEventListener('touchmove', this, false);
-      el.addEventListener('touchend', this, false);
-      if (!navigator.userAgent.match(/iPhone|iPad|iPod/i) && !navigator.userAgent.match(/Android/i)) {
-        document.addEventListener('mousedown', this, false);
-        document.addEventListener('mousemove', this, false);
-        document.addEventListener('mouseup', this, false);
+      el.addEventListener('touchstart', this.rotateStart, false);
+      el.addEventListener('touchmove', this.rotateMove, false);
+      el.addEventListener('touchend', this.rotateStop, false);
+      if (!navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)) {
+        document.addEventListener('mousedown', this.rotateStart, false);
+        document.addEventListener('mousemove', this.rotateMove, false);
+        document.addEventListener('mouseup', this.rotateStop, false);
       }
     },
     rotateStart: function(e) {
       e.preventDefault();
-      originX = innerWidth / 2;
-      originY = innerHeight / 2;
-      setTimeout(function() { movement = 0 }, 2000);
-      el.style.transitionDuration = '0ms';
-      el.style.transform = '';
-      if (e.touches) var startX = e.touches[0].pageX - originX,
-        startY = e.touches[0].pageY - originY;
-      else var startX = e.clientX - originX,
-        startY = e.clientY - originY;
-
+      mousedown = true, movement = 0, originX = innerWidth / 2, originY = innerHeight / 2;
+      el.style.transitionDuration = '', el.style.transform = '';
+      setTimeout(function(){movement=0},2000);
+      if (e.touches) var startX = e.touches[0].pageX - originX, startY = e.touches[0].pageY - originY;
+      else var startX = e.clientX - originX, startY = e.clientY - originY;
       startAngle = Math.atan2(startY, startX) - angle;
-      movement = 0;
     },
     rotateMove: function(e) {
-      originX = innerWidth / 2;
-      originY = innerHeight / 2;
-      if (e.touches) var dx = e.touches[0].pageX - originX,
-        dy = e.touches[0].pageY - originY;
-      else var dx = e.clientX - originX,
-        dy = e.clientY - originY;
-      angle = Math.atan2(dy, dx) - startAngle;
+      e.preventDefault();
+      if (e.type == 'mousemove' && !mousedown) return;
+      originX = innerWidth / 2, originY = innerHeight / 2;
+      if (e.touches) var dx = e.touches[0].pageX - originX, dy = e.touches[0].pageY - originY;
+      else var dx = e.clientX - originX, dy = e.clientY - originY;
+      angle = Math.atan2(dy, dx) - startAngle, movement = parseFloat(movement) + 0.02;
       el.style.transform = 'rotate(' + angle + 'rad)';
-      movement = parseFloat(movement) + 0.02;
     },
     rotateStop: function(e) {
-      angle = movement * 1000;
+      mousedown = false, angle = movement * 1000;
       if (movement < 0.5) {
         el.style.transitionDuration = '16000ms';
-        angle = angle * 5;
+        angle *= 5;
       } else el.style.transitionDuration = Math.round(movement * 6000) + 'ms';
       el.style.transform = 'rotate(' + angle + 'rad)';
       movement = 0;
     }
   };
-function loaded() { spinner.init() }
 addEventListener("load", function() {
   var h = a('header');
   for (i = 1; i < 7; i++) {
@@ -77,7 +55,7 @@ addEventListener("load", function() {
   }
   document.addEventListener('touchmove', function(e) { e.preventDefault() });
   if (navigator.standalone) document.querySelector('header').style.paddingTop = "15px";
-  setTimeout(loaded, 100)
+  setTimeout(function(){spinner.init()}, 100)
 }, true);
 var x = 0;
 setInterval(function() {
