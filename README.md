fd47e6f3e1f68d7b29ef76d940a917d4830d139a
Cameron Samuels <me@cameronsamuels.com>
2017-12-12 15:14:07 -0600

    (issue)Created an orphan branch for the extension
    This contributes to issue #20.

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the branch for the [Fidget Chrome extension](https://goo.gl/4g232x) and the [Fidget Firefox add-on](https://goo.gl/yzhXb9).
+
+For more information, use the [master branch](https://git.io/vdlzR).
---
+++ dist/s.js
@@ -0,0 +1,50 @@
+var c = 0, d, e = document.querySelector("img"), f = 0, g, h, k, l;
+function m() {
+  h = innerWidth / 2, k = innerHeight / 2, e.style.transitionDuration = "0",
+  e.addEventListener("touchstart", n), e.addEventListener("touchmove", p), e.addEventListener("touchend", q);
+  e.onmousedown = n, document.onmousemove = p, document.onmouseup = q;
+}
+function n(a) {
+  a.preventDefault();
+  l = new Date, d = !0, c = 0, h = innerWidth / 2, k = innerHeight / 2,
+  e.style.transitionDuration = "";
+  if (a.touches) {
+    var b = a.touches[0].pageX - h;
+    a = a.touches[0].pageY - k;
+  } else b = a.clientX - h, a = a.clientY - k;
+  g = Math.atan2(a, b) - f;
+}
+function p(a) {
+  a.preventDefault();
+  if ("mousemove" != a.type || d) {
+    h = innerWidth / 2, k = innerHeight / 2;
+    if (a.touches) {
+      var b = a.touches[0].pageX - h;
+      a = a.touches[0].pageY - k;
+    } else b = a.clientX - h, a = a.clientY - k;
+    f = Math.atan2(a, b) - g, c = parseFloat(c) + 10,
+    e.style.transform = "rotate(" + f + "rad)";
+  }
+}
+function q() {
+  var a = c / (new Date - l);
+  d = !1, f = 1000 * a;
+  30 < new Date - l && 20 < c && (e.style.transitionDuration = Math.round(30000 * a) + "ms",
+  e.style.transform = "rotate(" + f + "rad)");
+  c = 0;
+}
+addEventListener("load", function() {
+  var a = document.querySelector("div");
+  for (i = 1; 7 > i; i++) {
+    var b = document.createElement("img");
+    b.src = "img/" + i + ".png";
+    b.ontouchend = function(a) { e.src = a.target.src };
+    b.onmouseup = b.ontouchend, a.appendChild(b);
+  }
+  document.addEventListener("touchmove", function(a) { a.preventDefault() });
+  setTimeout(m, 100);
+}, !0);
+setInterval(function() { c = 0 }, 165),
+setInterval(function() { l = new Date }, 1000),
+document.oncontextmenu = function(e) { e.preventDefault() },
+document.ondragstart = function(e) { return false }
